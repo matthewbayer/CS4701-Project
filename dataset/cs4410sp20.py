c646f4d0f495320d3d45fb3decc365e178bf147a
Matt Bayer <meb287@cornell.edu>
2020-10-25 21:33:14 -0400

    ADD: cs4410 boiissss

---
+++ dataset/cs4410sp20.py
@@ -0,0 +1,331 @@
+data = ["""Review session posten on Canvas; extra office hours today
+Dear all,
+
+I hope you are doing well. As the subject says, I posted on Canvas yesterday's review session, and your amazing TA staff has volunteered to offer 4 more hours today.
+
+11:00 am to noon
+
+1:00 PM to 2:00 PM
+
+2:00PM to 3:00 PM
+
+3:00 PM to 4:00 PM
+
+Details here:
+
+https://www.cs.cornell.edu/courses/cs4410/2020sp/officehours/
+
+All the best!""",
+""" FInal exam time, length, guidelines for submission, etc. --- PLEASE READ THE ATTACHED DOCUMENT
+Dear all,
+
+this message has attached two files -- they are identical in content, and differ only in format (one is PDF, the other Word).
+
+They contain detailed guidelines for the final exam.
+
+I am on purpose not telling you much  about it in this text, (diabolical, I know...) so that you will read the instructions in the file: finding out on the day of the exam that there is something you don't understand would be stressful and burn useful time for you. 
+
+The  high order bit is that we are trying to make the logistics of taking the exam  as easy as possible. In particular, you will not have to be connected to the internet during the length of the exam, except for downloading and uploading the exam.
+
+Now, you may ask, why two files? Though we recommend, if possible, that you enter your answer in the fillable PDF we will provide, you will also have an option to complete your answers in Word, and then generate a PDF (and more) . The two different file allow you to test features that you may depend on t during the test  (e.g., whether your PDF reader has any trouble  filling a text box).
+
+Please take a moment NOW to think about how you want to take your exam (filling a PDF, using Word, using pen and paper) and make sure now that you have down all the logistics to execute on your plan.
+
+So, please read the attached guidelines. And, if some things are unclear, now is the time to ask (well, maybe tomorrow morning... :-) ) 
+
+Take good care,
+
+   L.
+
+FInal_Submission_Guidelines_.pdf
+
+FInal_Submission_Guidelines_.docx
+
+Please also  see @1210_f3 regarding the policy for exams submitted past the exam time.
+
+""",
+""" FInal exam time, length, guidelines for submission, etc. --- PLEASE READ THE ATTACHED DOCUMENT
+Dear all,
+
+this message has attached two files -- they are identical in content, and differ only in format (one is PDF, the other Word).
+
+They contain detailed guidelines for the final exam.
+
+I am on purpose not telling you much  about it in this text, (diabolical, I know...) so that you will read the instructions in the file: finding out on the day of the exam that there is something you don't understand would be stressful and burn useful time for you. 
+
+The  high order bit is that we are trying to make the logistics of taking the exam  as easy as possible. In particular, you will not have to be connected to the internet during the length of the exam, except for downloading and uploading the exam.
+
+Now, you may ask, why two files? Though we recommend, if possible, that you enter your answer in the fillable PDF we will provide, you will also have an option to complete your answers in Word, and then generate a PDF (and more) . The two different file allow you to test features that you may depend on t during the test  (e.g., whether your PDF reader has any trouble  filling a text box).
+
+Please take a moment NOW to think about how you want to take your exam (filling a PDF, using Word, using pen and paper) and make sure now that you have down all the logistics to execute on your plan.
+
+So, please read the attached guidelines. And, if some things are unclear, now is the time to ask (well, maybe tomorrow morning... :-) ) 
+
+Take good care,
+
+   L.
+
+FInal_Submission_Guidelines_.pdf
+
+FInal_Submission_Guidelines_.docx
+
+Please also  see @1210_f3 regarding the policy for exams submitted past the exam time.
+
+""",
+"""Two exams, with solutions
+Dear all,
+
+please find below two exams, with solutions. Note: Not all the questions apply to us!
+
+In particular, you should pay no attention (except if you are curious, of course!) to questions that have to with security/networking  (COVID aside, in our class we chose  to deeper on other aspects of the course that in previous years--given that the Department now offers classes in both security and networking).
+
+Nonetheless, I hope you find them useful!
+
+2017sp_4410_Prelim2.pdf
+
+2017sp_4410_Prelim2_soln.pdf
+
+2019sp_4410_final.pdf
+
+2019sp_4410_final_soln.pdf """,
+"""A4 Fixes
+This post contains the history of changes and bug fixes to A4 and will be updated as more changes occur.
+
+NOTE: Please periodically refer to this post and ensure that you're working with the latest version of the code from github.
+
+Apr 28: Fixed bug in the free_block function of inode_layer.cc
+Apr 28: Refine start_code/README.md for better explanation of the read_file and write_file functions
+May 4: Correct the confusing comment in write_file for better understanding
+
+A note about all of these fixes should also be in the README.md of A4's repository. """,
+""" New project A4 released
+Dear all,
+
+please find below a link that will take you to the Github repo for A4.
+
+https://github.coecis.cornell.edu/yz2327/A4
+
+This is the project that takes the place of Prelim 2 and is weighted the same as Prelim 2 previously was  (namely 30% of the total grade) .
+
+As I mentioned in responding to your questions in  @782, you have the option of counting for your credit any two of the following three : Prelim 1, A4, and the final.
+
+The project will give you some experience in manipulating inodes,  the persistent data structures that the file systems keeps for each file it stores.
+
+We are going to begin discussing file systems this week in class, but we want you to get started on this right away.  To get you going, Yunhao has (amazingly!) prepared a YouTube video that contains, we believe, all you need to attack the project starting tomorrow. You can find the video here:
+
+https://youtu.be/EO7nBmw-Scc
+
+and on Canvas under the Module "Intro to A4", which you should be able to reach from this link:
+
+https://canvas.cornell.edu/courses/11544/pages/a4-tutorial?module_item_id=435490
+
+You can also find a pointer to it in the handout for the project you’ll find in the repo.
+
+Office Hours
+
+Every project increases the request for office hours. Besides, this is a new  project: we have worked very hard to work out all the kinks, but we have been, so to speak, building the car while driving it: most TAs will be doing the project just a few hours before you.
+
+So, let’s talk about office hours.
+
+I heard from some of you that some office hours have been busy, to the point that you have not been able to get the help you were looking for.  I have discussed this with the teaching staff and we have been exchanging ideas on how to make things more efficient and give to as many students as possible a chance to ask at least one of their questions during office hours.
+
+But, let’s face it: the medium is inefficient: it is yet another thing that we have to recognize and, all of us, learn to live with: from the teaching staff side, it means trying to find ways to optimize the interactions; from your side, it means adapting your behavior so that you can get, in these circumstances, the best possible help.
+
+Which brings me to another thing that I have learned from my TAs: almost all their office hours leading to A3 were empty — just tin the two/three days before the assignment was due there was a spike.
+
+So, to recap:  1) you may need help; 2) office hours immediately before the deadline are unlikely to be able to absorb the load; 3) most office hours before the last bottleneck days are empty. 
+
+You are smart Cornell undergrads -- but here is a friendly hint ;-)
+
+       start on your project NOW, and beat the rush.
+
+And be done early: if you don’t  start thinking about it when the project is basically due and fall pray of the “fog of war”, it is a project you can finish relatively quickly.
+
+Due Date
+
+
+The project is due May 14, at 11:59 pm  Ithaca Time
+
+A public ‘Thank you”
+
+I have been touched by the dedication of the TAs in this class. Preparing a project from nothing takes hard work, and to do so while managing the challenging times we are in is truly spectacular. I realize that it is hard to be thankful to someone who has just created a new project for you to work on, but I hope you too appreciate their dedication.
+
+The best way to repay them for the hard work is to get on with your project soon (today), and, while working at it, to remind yourself that the honor system we live by is a covenant of free people, who, free to be other than honorable, choose instead to be so.""",
+""" Online Lecture and my new office hours
+Dear all,
+
+I hope this find you and your family well and safe. 
+
+We are about to restart our intellectual journey together: even I can see you rolling your eyes, I hope that deep inside (ok, perhaps really deep inside) you are excitedI
+
+I wanted to share with you how lectures are going to proceed.
+
+Lecture  -- Synchronous delivery
+
+Following the University's guidelines, I am going to deliver lectures synchronously over Zoom at our normal times (Tuesdays and Thursdays, from 1:25 to 2:40 pm EST).
+
+If you go to your calendar on Canvas, you should see entries for each of the classes, with a link that will allow you to join them. (You must be logged into Zoom via your Cornell ID: Choose the "Sign in via SSO" option in zoom.)
+
+Lecture  -- Asynchronous delivery
+
+Just as I did for the last lecture before our long recess, I will post both video and audio of the lectures in modules on Canvas.
+
+Zoom call etiquette
+
+Running a class of our size while trying to make it as interactive as possible is a challenge when we are together -- a virtual meeting makes it even more so. 
+
+Here is what we'll do:
+
+-  Please enter by clicking on the zoom link on Canvas -- that way, you'll be automatically authenticated. You need to authenticate yourself to join the call. 
+
+- At the beginning of the call you will be muted. You can decide whether to keep your video on or off. Please consider keeping your video on: it's weird enough to teach into the void--at least, seeing some of you will make it less awkward. :-) 
+
+- If you would like to ask a question, please click on the Participants tab and then click on the small icon in the shape of a hand. I will periodically stop during the class and go through questions, and will unmute you to give you a chance to ask yours.
+
+- Voicing your questions is the most effective way for us to focus on things that are unclear --but if you would rather not speak up, you can also ask me questions through chat. I will then read your question aloud and answer it.
+
+My Office Hours
+
+I will keep my 3:00 to 4:00 pm  office hour on Tuesdays, but I will move my office hour on Thursdays from 10:00 to 11:00 pm, to make it easier for those of you in a very different time zone.
+
+See you soon!""",
+""" Hi all,
+
+We will hold office hours at the times visible on the website. Some remain unchanged, some are updated, so take a look.
+
+To join online office hours please use the attached Zoom Link below. This meeting room is "open" all day, but will only be accessible to you if a TA is present - so take a look at Piazza in case a TA cancels his office hours short-term or is delayed. It will work as follows:
+
+TA's will admit you into the meeting room where you can queue up for help by using the "raise hand" feature. A TA will see this and service you in invocation order.  You start off muted, but may ask questions by text using the Chat feature "Message Host". Once it is your turn, a TA will place himself and you in a "Breakout" room, i.e. a private 1 on 1 chat, where you can share your screen and ask individual questions.
+
+Note, that you must be using your Cornell Zoom ID (Signin via SSO) to join.""",
+""" A4 and Final graded -- regrade requests close at noon Ithaca time on June 4th
+Dear all,
+
+I hope this finds all of you as well as we all can be in these very very painful  times.
+
+I am writing today about the final: it is graded, and so is A4. Final grades are due very soon, so we'll be able to accept regrade requests for 24 hours, until noon Ithaca time tomorrow June 4th .
+
+I would encourage you to be thoughtful when submitting regrade requests: fighting on one or two points is not going to make a difference on your final grade. Please only submit regrade requests if you believe that the grader was actually incorrect. 
+
+Stay strong and stay safe -- I hope to see many of you in person next semester,
+
+ Lorenzo""",
+""" partial credit on manual grading
+I've been stuck on part 2 for awhile. I keep getting 5/10 with not consistent with put large file error.
+I tried to multiple methods to debug and went to office hours but TAs weren't able to find the reason.
+I really tried my best and stayed up for 4 days in a row to go to office hours...
+Would I be able to get some credit on manual grading part even if I didn't get 10/10 on the autograder for part 2?
+
+Thanks!""",
+""" the answer is B.
+
+From our slides, I remember inverted page table is tied to pid, why don't we choose E?""",
+""" Sorry, this question takes a long time to answer. Office hours, of which I had plenty with no takers, would have been the right vehicle for it...
+
+As for the difference between available and resources, the name is a bit of a give away: the available resources are the resources that are currently available (sorry, I am not trying to be cute--they are really just that); the pending resources are resources that have been requested by the corresponding process, (the table encodes who requested what). If you understand what a safe sequence is (and I suspect you do!) then perhaps this is enough to get you going.""",
+""" Deadlock Detecting example in slide
+There is a detecting deadlock example in the slide 5.Deadlock. I am a little bit confused how it works. What is the difference between available resources and pending resources? And how the safe sequence works for this example?
+
+
+exam""",
+""" The statement counter += 1 is not atomic - it's actually more like READ + ADD + WRITE.
+
+The case for 3 is fairly straightforward - each thread runs sequentially, so we get 3 increments.
+
+For 1, let's say all of the threads execute their READ instructions at the same time. They all read 0. Each of the threads then adds to what they believe is the counter value of 0, and then writes 1 to the counter variable. The individual increments are thus lost; counter can be 1 because each thread has written the value 1 to wherever counter is in memory.
+
+Getting 2 can happen in an intermediary between the above two cases - two threads can both write 1, and the last one correctly increments to write 2.""",
+""" For the question d, why the possible values could be 1, 2, 3?""",
+""" I'm a little confused how thrashing occurs if WSi > # frames. I understand that the working set is the set of referenced mappings between pages and frames but don't fully understand how increasing the working set would result in more page faults""",
+""" The minimum is 2 for no-TLB, no-cache, single-level page table because we have to read a page of the page table to get a physical address and then read the data corresponding to the physical address. Without a TLB or a cache, we can't really improve this.
+
+In the two-level page table, no-TLB, user data cache scenario, the minimum is also 2; we read a page form each level of the page table to get a physical address, and then a cache hit stops us from performing the third memory access.
+
+In the remaining scenario, we can get a TLB hit, so we get a physical address from a virtual one without looking at memory. We can also get a cache hit, so we never visit memory at all. In the worst case, the data is not in the TLB so we have to walk the page table, and then we can have a cache miss for a total of 3 memory accesses.
+
+Hope this helps!""",
+""" could someone explain why the minimum is 2 for no tlb, no cache, single level? similarly, could someone please provide an explanation for the remaining two scenarios?
+
+""",
+""" In the slide above, how did we get the 6 bits (26 frames) in the calculation for the sizeofEntry?
+
+""",
+""" problem 2.ii on practice final
+for problem 2.ii on practice final, could someone explain this part of the solution: "The program's address space consists of 1024 pages, thus we need we need 16 third-level page tables. Therefore we need 16 entries in a 2nd level page table, and one entry in the first level page table. " ?""",
+""" As the answer stated, it will need the entire 2^10 top level translation table (use one of it and leaving the rest invalid) and 8 entries of the second-level. This means it should be 16KB(top level) + 8(number of entries) * 4byte(for each entry).
+Why the answer used one page for merely 8 entries rather than using the exact size? Should the memory page based here?""",
+""" Is there going to be an automatic drop in the lowest exam score?
+Do we need to formally email the faculties to drop one of our grades or it is going to be automatically done?""",
+"""why FCFS doesn't has the property of exploit locality?
+
+why Elevator doesn't provide fairness? """,
+""" Grade Weight Breakdown - Attempt 2
+I am copy-pasting @1063 because no TA or Professor has responded yet:
+
+How similar can we expect the grade breakdown to be to that presented us to the start of the course? Is it roughly going to still be:
+
+40% split between A1/A2/A3/R1+R2
+60% split between the highest two of A4/Prelim 1/Final
+Or will we have to expect substantial adjusting of weights?""",
+""" code doesn't work on linux server?
+My code got 40/40 for part1 on my local machine but just can't proceed in Linux?
+
+It just stops like below
+
+I've been working on my laptop (Mac) and it's working well...""",
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,
+""" """,]
